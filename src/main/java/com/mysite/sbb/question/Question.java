5d2c423b762a517bd566726ac9263ed672290638
Bang_Jun_Yeong <qkqhwjstk111@sunmoon.ac.kr>
2025-11-10 10:32:02 +0900

    2-06

--- src/main/java/com/mysite/sbb/question/Question.java
+++ src/main/java/com/mysite/sbb/question/Question.java
@@ -1,7 +1,9 @@
-package com.mysite.sbb;
+package com.mysite.sbb.question;
 
 import java.time.LocalDateTime;
 import java.util.List;
+
+import com.mysite.sbb.answer.Answer;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -13,20 +15,24 @@ import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
+
 @Getter
 @Setter
 @Entity
-public class Answer {
+public class Question {
 	
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(columnDefinition="TEXT")
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
     private String content;
 
-    private LocalDateTime createDate; 
-
+    private LocalDateTime createDate;
+    
     @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE) 
-    private List<Answer> answerList;  
+    private List<Answer> answerList; 
 }
